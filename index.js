48242048ccc31037bb01654a88a63b17e6802a2a
Helen Liutongco <bellpickle@gmail.com>
2017-11-09 20:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file.
+
+function lowerCaseDrivers(drivers) {
+  const newDrivers=drivers.map(function (name){
+    return name.toLowerCase();
+  })
+  return newDrivers;
+}
+
+
+function nameToAttributes(drivers) {
+  const newArray=drivers.map(function (name){
+    let firstNameString="";
+    let lastNameString="";
+    let newObject={};
+    let i=0;
+    while(name[i]!==" "){
+      ++i;
+    }
+    firstNameString=name.slice(0,i);
+    lastNameString=name.slice(i+1);
+    newObject["firstName"]=firstNameString;
+    newObject["lastName"]=lastNameString;
+    return newObject;
+  })
+  return newArray;
+}
+
+function attributesToPhrase(drivers){
+  const hometownArray=drivers.map(function (object){
+    const keysArray=Object.keys(object);
+    const name=object[keysArray[0]]
+    const hometown=object[keysArray[1]]
+    return `${name} is from ${hometown}`
+  })
+  return hometownArray;
+}
