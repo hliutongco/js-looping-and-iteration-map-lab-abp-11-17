76fb603941ec6b24261518d9e14ea6457a80ef71
Helen Liutongco <bellpickle@gmail.com>
2017-11-09 20:26:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file.
+//let drivers=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function lowerCaseDrivers(drivers) {
+  const newDrivers=drivers.map(function (name){
+    return name.toLowerCase();
+  })
+  return newDrivers;
+}
+
+// drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
+
+function nameToAttributes(drivers) {
+  const newArray=drivers.map(function (name){
+    let firstNameString="";
+    let lastNameString="";
+    let newObject={};
+    let i=0;
+    while(name[i]!==" "){
+      ++i;
+    }
+    firstNameString=name.slice(0,i);
+    lastNameString=name.slice(i+1);
+    newObject["firstName"]=firstNameString;
+    newObject["lastName"]=lastNameString;
+    return newObject;
+  })
+  return newArray;
+}
